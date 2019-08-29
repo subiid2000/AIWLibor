7312180b7220c0ba74f1ebfda00880ccb4a51f7f
jojovarghese1 <jojo.varghese@reflectionsinfos.com>
2019-08-29 16:29:39 +0530

    app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { ConfirmationDialogService } from './containers/confirmation-dialog/conf
 import { UploadFilesComponent } from './views/upload-files/upload-files.component';
 import { NgSelectModule, SELECTION_MODEL_FACTORY } from '@ng-select/ng-select';
 import { FileUploadModule } from '@iplab/ngx-file-upload';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -60,7 +61,10 @@ import { FileUploadModule } from '@iplab/ngx-file-upload';
     MatMenuModule,
     MatCheckboxModule,
     NgSelectModule,
-    FileUploadModule
+    FileUploadModule,
+    NgbModule,
+    ModalModule.forRoot(),
+    TooltipModule.forRoot()
   ],
   providers: [ConfigurationsService, ConfirmationDialogService, PositioningService, ComponentLoaderFactory, ToasterService, KeycloakService, BsModalService,
     AuthGuardService, BsModalRef,  { provide: 'Window',  useValue: window },
